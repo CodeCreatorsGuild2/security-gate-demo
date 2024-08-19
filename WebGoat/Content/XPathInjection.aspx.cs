ec4edfcc29045033df2efa6b78c92ff45458b5b5
Cierkowski, Michael <michael.cierkowski@contractor.tranetechnologies.com>
2024-08-19 13:15:12 -0700

    tEST

--- WebGoat/Content/XPathInjection.aspx.cs
+++ WebGoat/Content/XPathInjection.aspx.cs
@@ -25,7 +25,7 @@ namespace OWASP.WebGoat.NET
         {
             XmlDocument xDoc = new XmlDocument();
             xDoc.LoadXml(xml);
-            XmlNodeList list = xDoc.SelectNodes("//salesperson5[state='" + state + "']");
+            XmlNodeList list = xDoc.SelectNodes("//salesperson5W[state='" + state + "']");
             if (list.Count > 0)
             {
 
